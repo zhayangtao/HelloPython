e39a95c0ce324f25cb3d81de179d5654c5570a8b
zhayangtao <15313039096@163.com>
2017-04-23 10:17:04 +0800

    提交文件

---
+++ python01/PythonBasic.py
@@ -0,0 +1,23 @@
+a = 100
+if a >= 0:
+    print(a)
+else:
+    print(-a)
+
+print(True and True)
+print(10/3)
+
+classmates = ['Michael','Bod','Tracy']
+print(len(classmates))
+classmates.insert(1,'Jack')
+
+if a >= 3:
+    print('adult')
+elif a >= 5:
+    print('child')
+else:
+    None
+
+for name in classmates:
+    print(name)
+
