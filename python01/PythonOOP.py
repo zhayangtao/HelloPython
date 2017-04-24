fe57452034302c248c1d9bc4a92d6162f8979f2e
zhayangtao <15313039096@163.com>
2017-04-24 15:08:52 +0800

    submit change

--- python01/PythonOOP.py
+++ python01/PythonOOP.py
@@ -78,8 +78,10 @@ type(123) == int
 # 使用 types模块定义的常量判断一个对象是否是函数
 import types
 
+
 def fn():
     pass
+
 
 type(fn) == types.FunctionType  # True
 type(abs) == types.BuiltinFunctionType  # True
@@ -96,12 +98,15 @@ isinstance([1, 2, 3], (list, tuple))
 # 使用dir获取对象所有属性和方法
 dir('ABC')
 
+
 class MyDog(object):
     def __len__(self):
         return 100
 
     def act(self):
         print('woof')
+
+
 dog = MyDog()
 print(len(dog))  # 100
 
@@ -120,14 +125,21 @@ fn()
 # 给实例绑定一个属性
 class Student(object):
     pass
+
+
 s = Student()
 s.name = 'Mike'
 print(s.name)
+
+
 # 给实例绑定给一个方法
-def set_age(self, age): # 定义一个函数作为实例方法
+def set_age(self, age):  # 定义一个函数作为实例方法
     self.age = age
+
+
 from types import MethodType
-s.set_age = MethodType(set_age, s) # 给实例绑定一个方法
+
+s.set_age = MethodType(set_age, s)  # 给实例绑定一个方法
 s.set_age(34)
 print(s.age)
 # 给一个实例绑定的方法，对另一个实例是不起作用的
@@ -143,12 +155,146 @@ Student.set_age = set_age
 # __slots__定义的属性仅对当前类实例起作用，对继承的子类是不起作用的
 class Student(object):
     __slots__ = ('name', 'age')  # 用tuple定义允许绑定的属性名称
+
+
 s = Student()
 s.name = 'jojo'
 s.age = 34
-s.score = 88  # 'Student' object has no attribute 'score'
+
+
+# s.score = 88  # 'Student' object has no attribute 'score'
+
 
 class GradStudent(Student):
     pass
+
+
 g = GradStudent()
 g.score = 666
+
+
+# 使用 @property
+class Student(object):
+    def get_score(self):
+        return self.get_score()
+
+    def set_score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('Score must be an integer')
+        if value < 0 or value > 100:
+            raise ValueError('Score must between 0 ~ 100!')
+        self._score = value
+
+
+# Python内置的 @property 装饰器就是负责把一个方法变成属性调用的：
+class Student(object):
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('Score must be an integer')
+        if value < 0 or value > 100:
+            raise ValueError('Score must between 0 ~ 100!')
+        self._score = value
+
+
+# 还可以定义只读属性，只定义getter方法，不定义setter方法
+class Student(object):
+    @property
+    def birth(self):
+        return self._birth
+
+    @birth.setter
+    def birth(self, value):
+        self._birth = value
+
+    @property
+    def age(self):
+        return 2017 - self._birth
+
+
+# 多重继承
+class Runnable(object):
+    def run(self):
+        print('Running')
+
+
+class Flyable(object):
+    def fly(self):
+        print('Flying')
+
+
+class Chicken(Runnable, Flyable):
+    pass
+
+
+# MixIn
+
+
+# 定制类
+# __str__
+class Student(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return 'Student object (name: %s)' % self.name
+
+
+print(Student('Michael'))  # Student object (name: Michael)
+s = Student('Micheal')
+s  # <__main__.Student object at 0x109afb310>
+
+
+# 直接显示变量调用的不是__str__()，而是__repr__()
+class Student(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return 'Student object (name: %s)' % self.name
+
+    __repr__ = __str__
+
+
+# __iter__ 如果一个类想被用于for ... in循环，类似list或tuple那样，
+# 就必须实现一个__iter__()方法，该方法返回一个迭代对象
+# 然后，Python的for循环就会不断调用该迭代对象的__next__()方法，直到遇到StopIteration错误时退出循环
+class Fib(object):
+    def __init__(self):
+        self.a, self.b = 0, 1
+
+    def __iter__(self):
+        return self  # 实例本身就是迭代对象，故返回自己
+
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b
+        if self.a > 1000:
+            raise StopIteration()
+        return self.a
+for n in Fib():
+    print(n)
+
+
+# __getitem__ 要表现得像list那样按照下标取出元素，需要实现__getitem__()方法：
+class Fib(object):
+    def __getitem__(self, item):
+        a, b = 1, 1
+        for x in range(item):
+            a, b = b, a + b
+        return a
+f = Fib()[12]
+print(f)
+
+class Fib(object):
+    def __getitem__(self, item):
+        if isinstance(item, int):
+            a, b = 1, 1
+            for x in range(item):
+                a, b = b, a + b
+            return a
+        if isinstance(item, slice):
+            
