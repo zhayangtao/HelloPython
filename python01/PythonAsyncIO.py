6b9e530c67b4791e94078a663def285ee082b969
zhayangtao <15313039096@163.com>
2017-04-28 08:28:11 +0800

    submit new files and change

--- python01/PythonAsyncIO.py
+++ python01/PythonAsyncIO.py
@@ -1,35 +1,14 @@
-#  协程
-def consumer():
-    r = ''
-    while True:
-        n = yield r
-        if not n:
-            return
-        print('[CONSUMER] Consuming %s' % n)
-        r = '200 OK'
-
-
-def produce(c):
-    c.send(None)
-    n = 0
-    while n < 5:
-        n += 1
-        print('[PRODUCER] Producing %s' % n)
-        r = c.send(n)
-        print('[PRODUCER] Consumer return: %s' % r)
-    c.close()
-
-
-c = consumer()
-produce(c)
-
 import asyncio
+
+
 @asyncio.coroutine
 def hello():
-    print('Hello, world')
+    print('Hello world')
     # 异步调用
     r = yield from asyncio.sleep(1)
     print('Hello again')
+
+
 # 获取 Eventloop
 loop = asyncio.get_event_loop()
 # 执行
@@ -41,7 +20,9 @@ loop.close()
 async def hello():
     print('Hello world')
     r = await asyncio.sleep(1)
-    print('Hello agein')
+    print('Hello again')
+
+
 # 获取 Eventloop
 loop = asyncio.get_event_loop()
 # 执行
