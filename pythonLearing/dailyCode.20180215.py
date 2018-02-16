e1f9d7ecf265dd6cfa74d64ef8ca32698a569086
zhayangtao <1264214725@qq.com>
2018-02-16 10:08:40 +0800

    提交20180215日常代码

---
+++ pythonLearing/dailyCode.20180215.py
@@ -0,0 +1,44 @@
+x = set('spam')
+print(x)
+x = set('spam')
+print(x)
+x = {'spam'}
+print(x)
+
+import decimal
+
+temp = decimal.getcontext().prec = 2 
+decimal.getcontext().prec = temp
+
+
+import math
+
+math.sqrt(144)
+
+
+import random
+
+random.random()
+random.choice(['life of brian', 'holy shit', 'meaning of life'])
+
+print(0.1 + 0.1 + 0.1 - 0.3) # 缺失精度
+
+from decimal import Decimal
+a = Decimal(0.1) + Decimal(0.1) + Decimal(0.1) - Decimal(0.3)
+print(a) # 缺失精度
+b = Decimal('0.1') + Decimal('0.1') + Decimal('0.1') - Decimal('0.3')
+print(b)
+
+with decimal.localcontext() as ctx:
+    ctx.prec = 2
+    print(decimal.Decimal('1.00') / decimal.Decimal('3.00'))
+
+
+if True:
+    def func():
+        pass
+else:
+    def func():
+        pass
+
+
