29f046d0cfdc4dfd4ae5d69e3628920e8f375d78
zhayangtao <17600102144@163.com>
2018-02-26 22:29:26 +0800

    提交20180226代码

---
+++ pythonLearning/dailyCode_20180226.py
@@ -0,0 +1,44 @@
+def f(a, *b, c=6, **d):
+    print(a, b, c, d)
+
+
+f(1, *(2, 3), **dict(x=4, y=5))
+
+
+def min1(*args):
+    res = args[0]
+    for arg in args[1:]:
+        if arg < res:
+            res = arg
+    return res
+
+
+def min2(first, *rest):
+    for arg in rest:
+        if arg < first:
+            first = arg
+    return first
+
+
+def min3(*args):
+    tmp = list(args)
+    tmp.sort()
+    return tmp[0]
+
+
+def minmax(test, *args):
+    res = args[0]
+    for arg in args[1:]:
+        if test(arg, res):
+            res = arg
+    return res
+
+
+def lessthan(x, y):
+    return x < y
+
+
+def grtthan(x, y):
+    return x > y
+
+
