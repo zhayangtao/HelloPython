dea1866e3eae794f399ff9508c284b844ac56af9
zhayangtao <1264214725@qq.com>
2018-03-05 14:06:10 +0800

    排序 import

--- pythonLearning/dailyCode_20180304.py
+++ pythonLearning/dailyCode_20180304.py
@@ -1,5 +1,6 @@
-import time
 import sys
+import time
+
 [x + y for x in [1, 2, 3] for y in [4, 5, 6]]
 {x + y for x in [1, 2, 3] for y in [4, 5, 6]}
 {x: y for x in [1, 2, 3] for y in [4, 5, 6]}
@@ -50,5 +51,3 @@ for test in (forLoop, listComp, mapCall, genExpr, genFunc):
     print('-' * 33)
     print('%-9s: %.5f => [%s...%s]' %
           (test.__name__, elapsed, result[0], result[-1]))
-
-
