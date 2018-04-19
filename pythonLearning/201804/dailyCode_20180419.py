dfdc0731c688f0bfdf86db0a09aa43272fe5cd48
zhayangtao <1264214725@qq.com>
2018-04-19 22:51:07 +0800

    提交20480419日常代码

---
+++ pythonLearning/201804/dailyCode_20180419.py
@@ -0,0 +1,120 @@
+"""
+python 多线程
+"""
+"""
+import os
+from multiprocessing import Process
+print('Process (%s) start...' % os.getpid())
+
+
+def run_proc(name):
+    print('Run child process %s (%s)...' % (name, os.getpid()))
+
+
+if __name__ == '__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Process(target=run_proc, args=('test',))
+    print('Child process will start.')
+    p.start()
+    p.join()
+    print('Child process end.')
+"""
+
+"""
+进程池
+"""
+
+
+from multiprocessing import Pool
+import os
+import time
+import random
+
+
+def long_time_task(name):
+    print('Run task %s (%s)...' % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs % .2f second.' % (name, (end - start)))
+
+
+if __name__ == '__main__':
+    print('_' * 20)
+    print('Parent process %s.' % os.getpid())
+    p = Pool(4)
+    for i in range(5):
+        p.apply_async(long_time_task, args=(i,))
+    print('Waiting for all subprocessed done...')
+    p.close()
+    p.join()
+    print('All subprocess done.')
+
+
+"""
+子进程
+"""
+
+import subprocess
+
+print('$ nslookup www.python.org')
+r = subprocess.call(['nslookup', 'www.python.org'])
+print('Exit code:', r)
+
+
+"""
+进程间通信
+"""
+
+from multiprocessing import Process, Queue
+
+
+def write(q):
+    print('Process to write: %s' % os.getpid())
+    for value in ['A', 'B', 'C']:
+        print('Put %s to queue...' % value)
+        q.put(value)
+        time.sleep(random.random())
+
+
+def read(q):
+    print('Process to read: %s' % os.getpid())
+    while True:
+        value = q.get(True)
+        print('Get %s from queue.' % value)
+
+
+if __name__ == '__main__':
+    q = Queue()
+    pw = Process(target=write, args=(q,))
+    pr = Process(target=read, args=(q,))
+    pw.start()
+    pr.start()
+    pw.join()
+    pr.terminate()
+
+
+"""
+多线程
+"""
+
+import threading
+
+# 新线程执行的代码
+
+
+def loop():
+    print('thread %s is running...' % threading.current_thread().name)
+    n = 0
+    while n < 5:
+        n += 1
+        print('thread %s >>> %s' % (threading.current_thread().name, n))
+        time.sleep(1)
+    print('thread %s ended.' % threading.current_thread().name)
+
+
+print('thread %s is running...' % threading.current_thread().name)
+t = threading.Thread(target=loop, name='LoopThread')
+t.start()
+t.join()  # 等待线程结束
+print('thread %s ended.' % threading.current_thread().name)
